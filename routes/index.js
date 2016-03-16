24b0344e418ac4072827a8002109ab56fcbcfdff
Jéssika Renally Ribeiro Rodrigues <reh.nally@gmail.com>
2016-03-16 00:29:36 -0300

    crianco a livrria

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendfile('index.html');
+  res.sendfile('www/views/livraria.html');
 });
 
 module.exports = router;
