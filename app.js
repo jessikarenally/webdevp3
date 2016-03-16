618acac6a9ce7a55ba8d2b917b181186285b28a2
jessikarenally <reh.nally@gmail.com>
2016-03-16 13:15:10 -0300

    revertendo app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
 
 var routes = require('./routes/index');
 
@@ -12,5 +14,29 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, 'www')));
 
 app.use('/', routes);
+
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+if (app.get('env') === 'development') {
+ app.use(function(err, req, res, next) {
+	 res.status(err.status || 500);
+	 res.render('error', {
+	 message: err.message,
+	 error: err
+ 	});
+ });
+}
+
+app.use(function(err, req, res, next) {
+ res.status(err.status || 500);
+ res.render('error', {
+ message: err.message,
+ error: {}
+ });
+});
 
 module.exports = app;
